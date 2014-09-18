c43d2e7e34d221a334a96a5d27c5acf0860003ff
Mariusz Gronczewski <xani666@gmail.com>
2014-09-19 01:43:23 +0200

    some formatting

--- zeroctl.go
+++ zeroctl.go
@@ -9,17 +9,17 @@ import (
 var log = logging.MustGetLogger("example")
 
 //var format = "%{color}%{time:2006-01-02T15:04:05.9999Z-07:00} → %{level:.4s} %{id:03x}%{color:reset} %{message}"
-var format = "%{color:bold}%{time:2006-01-02T15:04:05.9999Z-07:00}%{color:reset}%{color} [%{level:.1s}] %{color:reset}%{shortpkg}[%{longfunc}] %{message}"
+var stdout_log_format = "%{color:bold}%{time:2006-01-02T15:04:05.9999Z-07:00}%{color:reset}%{color} [%{level:.1s}] %{color:reset}%{shortpkg}[%{longfunc}] %{message}"
+var syslog_log_format = "[%{level:.1s}] {shortpkg}[%{longfunc}] %{message}"
 
 func main() {
 	logBackend := logging.NewLogBackend(os.Stderr, "", 0)
-	syslogBackend, err := logging.NewSyslogBackend("")
+	syslogBackend, err := logging.NewSyslogBackend("zeroctl")
 	if err != nil {
 		log.Fatal(err)
 	}
 	logging.SetBackend(logBackend, syslogBackend)
-	logging.SetFormatter(logging.MustStringFormatter(format))
+	logging.SetFormatter(logging.MustStringFormatter(stdout_log_format))
 	log.Error("Logging")
 	log.Warning("Works")
-
 }

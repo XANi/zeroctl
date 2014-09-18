42308a092e4de1c86a5603feaf2c01d4b8ba2f59
Mariusz Gronczewski <xani666@gmail.com>
2014-09-19 01:28:55 +0200

    bootstrap some decent logging format

--- zeroctl.go
+++ zeroctl.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"os"
-	"fmt"
+	//	"fmt"
 	"github.com/op/go-logging"
+	"os"
 )
 
-
 var log = logging.MustGetLogger("example")
-var format = "%{color}%{time:15:04:05.000000} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}"
 
+//var format = "%{color}%{time:2006-01-02T15:04:05.9999Z-07:00} → %{level:.4s} %{id:03x}%{color:reset} %{message}"
+var format = "%{color:bold}%{time:2006-01-02T15:04:05.9999Z-07:00}%{color:reset}%{color} [%{level:.1s}] %{color:reset}%{shortpkg}[%{longfunc}] %{message}"
 
 func main() {
 	logBackend := logging.NewLogBackend(os.Stderr, "", 0)
-    syslogBackend, err := logging.NewSyslogBackend("")
+	syslogBackend, err := logging.NewSyslogBackend("")
 	if err != nil {
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
 	logging.SetBackend(logBackend, syslogBackend)
-    logging.SetFormatter(logging.MustStringFormatter(format))
-	fmt.Printf("Hello, world\n")
-	log.Error("err")
+	logging.SetFormatter(logging.MustStringFormatter(format))
+	log.Error("Logging")
+	log.Warning("Works")
 
 }
